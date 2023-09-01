10a07f6e3a2ee1b213670857103d0c873bce7de0
Ashu Singh <ashu.singh2001.as@gmail.com>
2023-09-01 11:46:33 +0530

    Maven
    Maven Hands-On

---
+++ MysqlJDBCProject/src/main/java/org/apache/maventraining/App.java
@@ -0,0 +1,13 @@
+package org.apache.maventraining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
